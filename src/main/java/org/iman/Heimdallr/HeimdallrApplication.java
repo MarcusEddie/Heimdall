b77a0db423cdaff5da0c9995ec76751541e980e2
NRL <yulee92@163.com>
2021-08-12 22:12:39 +0800

    upgrage java version to 11

---
+++ src/main/java/org/iman/Heimdallr/HeimdallrApplication.java
@@ -0,0 +1,13 @@
+package org.iman.Heimdallr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeimdallrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeimdallrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/iman/Heimdallr/HeimdallrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iman.Heimdallr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeimdallrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
